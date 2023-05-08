dfd7c6b7599a44a0073d035232638b12d0c15753
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-09 00:32:50 +0600

    cors middleware  added

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+const users = [
+    { id: 1, name: "Shabana", email: "shabana@gmail.com" },
+    { id: 2, name: "Shabnur", email: "shabnur@gmail.com" },
+    { id: 3, name: "Sabila", email: "sabila@gmail.com" },
+]
+
 app.get('/', (req, res) => {
     res.send('Users management server is running.');
+})
+
+app.get('/users', (req, res) => {
+    res.send(users);
 })
 
 app.listen(port, () => {
